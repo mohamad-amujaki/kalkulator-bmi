04ea95725f65a461e925b0be3c865b76af03f0a0
Mohamad Arif Mujaki <mohamad.amujaki@gmail.com>
2026-05-17 05:59:18 +0700

    Show calculate BMI if name >= 3 caracters

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Kalkulator BMI Interaktif")
 # Membuat input nama pengguna
 nama = st.text_input("Masukkan Nama Anda")
 
-# Membuat input jenis kelamin
+# Membuat input jenis kelamin (menggunakan st.selectbox atau st.radio)
 jenis_kelamin = st.selectbox("Pilih Jenis Kelamin", ["Laki-laki", "Perempuan"])
 
 # Membuat input angka untuk berat badan
@@ -22,18 +22,19 @@ tinggi_m = tinggi / 100
 # Menghitung BMI
 bmi = berat / (tinggi_m ** 2)
 
-# Menampilkan hasil perhitungan BMI ke layar
-st.success(f"BMI Anda adalah: {bmi:.2f}")
-
-if bmi < 18.5:
-    KATEGORI = "Berat Badan Kurang (Underweight)"
-    st.warning(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
-elif 18.5 <= bmi < 24.9:
-    KATEGORI = "Berat Badan Ideal (Normal)"
-    st.success(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
-elif 25 <= bmi < 29.9:
-    KATEGORI = "Berat Badan Berlebih (Overweight)"
-    st.warning(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
-else:
-    KATEGORI = "Obesitas"
-    st.error(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
+# Tampilkan perhitungan BMI jika nama telah terisi minimal 3 huruf
+if len(nama) >= 3:
+    # Menampilkan hasil perhitungan BMI ke layar
+    st.success(f"BMI Anda adalah: {bmi:.2f}")
+    if bmi < 18.5:
+        KATEGORI = "Berat Badan Kurang (Underweight)"
+        st.warning(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
+    elif 18.5 <= bmi < 24.9:
+        KATEGORI = "Berat Badan Ideal (Normal)"
+        st.success(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
+    elif 25 <= bmi < 29.9:
+        KATEGORI = "Berat Badan Berlebih (Overweight)"
+        st.warning(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
+    else:
+        KATEGORI = "Obesitas"
+        st.error(f"Predikat BMI Sdr. {nama} saat ini {KATEGORI}")
